197ccb9ebcadf21e675128abbf8da5b29200078f
Dominik Mrówka <dmrowka7@gmail.com>
2022-05-18 21:48:46 +0200

    Add Card and CardFrom

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,10 +1,16 @@
 import style from './Column.module.scss';
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 
 
 const Column = (props) => {
      return (
           <article className={style.column}>
                <h2 className={style.title}><span className={`${style.icon} fa fa-${props.icon}`} />{props.title}</h2>
+               <ul className={style.cards}>
+                    {props.cards.map(card => <Card key={card.id} title={card.title}/>)}
+               </ul>
+               <CardForm columnId={props.id} />
           </article>
      );
 };
