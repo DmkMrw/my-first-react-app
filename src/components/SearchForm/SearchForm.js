7d4972742325b859cf490a0be8e0d21ff22714cc
Dominik Mrówka <dmrowka7@gmail.com>
2022-05-29 18:50:16 +0200

    Add action searching

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,24 @@
 import style from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+
+  const [searchValue, setSearchValue] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload:  searchValue  });
+    console.log('ok');
+  }
+
   return (
-    <form className={style.searchForm}>
-      <TextInput placeholder="Search..."/>
+    <form className={style.searchForm} onSubmit={handleSubmit}>
+      <TextInput placeholder="Search..." value={searchValue} onChange={e => setSearchValue(e.target.value)} />
       <Button>
         <span className="fa fa-search"></span>
       </Button>
