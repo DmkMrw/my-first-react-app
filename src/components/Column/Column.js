dcf392dbd02df8fba9f8f39178c32caffd0b121c
Dominik Mrówka <dmrowka7@gmail.com>
2022-05-29 19:37:54 +0200

    Add action creators for addCard and updateSearchString

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -10,6 +10,7 @@ import { getFilteredCards } from '../../redux/store';
 const Column = (props) => {
 
      const cards = useSelector(state => getFilteredCards(state, props.id));
+     console.log('Column render');
 
 
      return (
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,6 +3,7 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
 
@@ -12,8 +13,7 @@ const SearchForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: 'UPDATE_SEARCHSTRING', payload:  searchValue  });
-    console.log('ok');
+    dispatch(updateSearchString( searchValue ));
   }
 
   return (
